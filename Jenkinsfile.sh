0cc47c0c5c4ee030acb5b8ab2794364e441d4953
gyanid <54573564+gyanid@users.noreply.github.com>
2019-09-20 13:34:00 +0530

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,13 +1,13 @@
 
 pipeline {
     agent any
-//	tools {
-//		maven 'maven3.6'
-//	}
-//	environment 
-// 	{
-// 	M2_HOME="/home/jjenkin/software/apache-maven-3.6.2"
-//}
+	tools {
+		maven 'maven3.6'
+	}
+	environment 
+ 	{
+ 	M2_HOME="/home/jjenkin/software/apache-maven-3.6.2"
+        }
 
 
     stages {
